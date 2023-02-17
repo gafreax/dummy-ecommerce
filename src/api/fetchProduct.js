3c52b63deddf8594c6fa8c88a7e3a80b38e8d9e9
Gabriele Fontana <gafreax@gmail.com>
2023-02-17 20:34:33 +0100

    fix: fetchProduct

--- src/api/fetchProduct.js
+++ src/api/fetchProduct.js
@@ -1,8 +1,10 @@
 import { API_BASE_URL, API_FETCH_LIMIT } from "../config.js";
 
-const fetchProducts = async ({skip, searchText}) => {
-    const searchFilter = searchText ? `/search?q=${searchText}`:'';
-    const url = `${API_BASE_URL}products${searchFilter}?limit=${API_FETCH_LIMIT}&skip=${skip}`;
+const fetchProducts = async ({skip, searchText=null}) => {
+    const searchFilter = searchText ? `/search?q=${searchText}&`:'/?';
+    console.log("fetchProducts: searchFilter", searchFilter);
+    const url = `${API_BASE_URL}products${searchFilter}limit=${API_FETCH_LIMIT}&skip=${skip}`;
+    console.log("fetchProducts: url", url)
     const dataFetched = await fetch(url);
     const dataJSON = await dataFetched.json();
     return dataJSON;
