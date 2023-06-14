10d92fb4908ce877857d20016cf85a3d7572a131
Giulia s <g.schillaci@live.it>
2023-06-14 20:33:32 +0200

    stile bottoni quantità

--- src/components/Categories/CategoriesMobile/index.js
+++ src/components/Categories/CategoriesMobile/index.js
@@ -1,6 +1,6 @@
 import React, { useMemo, useState } from "react";
 import { useDispatch } from "react-redux";
-import { Accordion,Badge } from "react-bootstrap";
+import { Accordion, Badge } from "react-bootstrap";
 
 import "../style.scss"
 
@@ -9,32 +9,32 @@ import fetchCategoryProducts from "../../../api/dummyjson/fetchCategoryProducts.
 
 const showCategories = (categories, setSelectedCategory) => {
   return categories?.map(category => {
-      return <Badge className="badge-categories m-1" key={`cat-${category}`} onClick={() => setSelectedCategory(category)}>
-        {category}
-      </Badge>
+    return <Badge className="badge-categories m-1 p-3" key={`cat-${category}`} onClick={() => setSelectedCategory(category)}>
+      {category}
+    </Badge>
   });
 };
 
-function CategoriesMobile ({categories}) {
-    const [selectedCategory, setSelectedCategory] = useState("");
+function CategoriesMobile({ categories }) {
+  const [selectedCategory, setSelectedCategory] = useState("");
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useMemo(() => {
-      if(selectedCategory && selectedCategory.length > 0) {
-        fetchCategoryProducts({dispatch, category: selectedCategory});
-      }
-    }, [dispatch, selectedCategory]);
+  useMemo(() => {
+    if (selectedCategory && selectedCategory.length > 0) {
+      fetchCategoryProducts({ dispatch, category: selectedCategory });
+    }
+  }, [dispatch, selectedCategory]);
 
 
-    return <Accordion >
+  return <Accordion >
     <Accordion.Item eventKey="0">
       <Accordion.Header>Categories</Accordion.Header>
       <Accordion.Body>
-      { showCategories(categories,setSelectedCategory) }
+        {showCategories(categories, setSelectedCategory)}
       </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
+    </Accordion.Item>
+  </Accordion>
 }
 
 export default CategoriesMobile;
